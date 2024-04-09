8b6cf0cdef43e06024b892e1bacdcb29e302e6ee
sandip95-web <sandipmaharjan95@gmail.com>
2024-04-09 16:28:03 +0545

    create search Api route

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -35,13 +35,11 @@ exports.updateListing = catchAysncError(async (req, res, next) => {
     req.body,
     { new: true }
   );
-  res
-    .status(200)
-    .json({
-      success: true,
-      message: "Listing deleted successfully",
-      updateListing,
-    });
+  res.status(200).json({
+    success: true,
+    message: "Listing deleted successfully",
+    updateListing,
+  });
 });
 
 exports.getListing = catchAysncError(async (req, res, next) => {
@@ -50,4 +48,49 @@ exports.getListing = catchAysncError(async (req, res, next) => {
     next(new ErrorHandler("Listing not found", 404));
   }
   res.status(200).json(listing);
+});
+
+exports.getListings = catchAysncError(async (req, res, next) => {
+  const limit = parseInt(req.query.limit) || 9;
+  const startIndex = parseInt(req.query.startIndex) || 0;
+  let offer = req.query.offer;
+
+  if (offer === undefined || offer === "false") {
+    offer = { $in: [false, true] };
+  }
+
+  let furnished = req.query.furnished;
+
+  if (furnished === undefined || furnished === "false") {
+    furnished = { $in: [false, true] };
+  }
+
+  let parking = req.query.parking;
+
+  if (parking === undefined || parking === "false") {
+    parking = { $in: [false, true] };
+  }
+
+  let type = req.query.type;
+
+  if (type === undefined || type === "all") {
+    type = { $in: ["sale", "rent"] };
+  }
+
+  const searchTerm = req.query.searchTerm || "";
+  const sort = req.query.sort || "createdAt";
+  const order = req.query.order || "desc";
+
+  const listings = await Listing.find({
+    name: { $regex: searchTerm, $options: "i" },
+    offer,
+    furnished,
+    parking,
+    type,
+  })
+    .sort({ [sort]: order })
+    .limit(limit)
+    .skip(startIndex);
+
+  return res.status(200).json(listings);
 });
--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const { verifyToken } = require("../middleware/verifyToken");
-const { createListing, deleteListing, updateListing, getListing} = require("../controllers/listingController");
+const {
+  createListing,
+  deleteListing,
+  updateListing,
+  getListing,
+  getListings,
+} = require("../controllers/listingController");
 
 const router = express.Router();
 
 router
   .post("/create", verifyToken, createListing)
   .delete("/delete/:id", verifyToken, deleteListing)
-  .put('/update/:id',verifyToken,updateListing)
-  .get("/get/:id",getListing);
+  .put("/update/:id", verifyToken, updateListing)
+  .get("/get/:id", getListing)
+  .get("/get", getListings);
 
 module.exports = router;
